dd5913ab6942b5d5dfb84986a05ffb0c45bb80a2
猫头猫 <31655147+maotoumao@users.noreply.github.com>
2023-10-27 12:50:30 +0800

    Update generate-code.ts

--- src/generate-code.ts
+++ src/generate-code.ts
@@ -1,18 +1,30 @@
 import ts from 'typescript';
 import path from 'path';
 import url from 'url';
-type c = JSX.IntrinsicAttributes
+import fs from 'fs/promises';
 
 
 const srcFile = path.resolve(url.fileURLToPath(import.meta.url), '../react-elements.ts');
-console.log(srcFile);
 const program = ts.createProgram([srcFile], {
     target: ts.ScriptTarget.ES2016
 });
 
 const sourceFile = program.getSourceFile(srcFile);
 
-(sourceFile.statements[0] as ts.InterfaceDeclaration).members.forEach((it: ts.PropertySignature) => {
-    console.log(it.name.getText(sourceFile));
-    console.log(it.type.getText(sourceFile));
-})
+await fs.mkdir("./generated-code/lower", {
+    recursive: true
+});
+await fs.mkdir("./generated-code/upper", {
+    recursive: true
+});
+
+(sourceFile.statements[0] as ts.InterfaceDeclaration).members.forEach(async (it: ts.PropertySignature) => {
+    const compName = it.name.getText(sourceFile);
+    const compProps = it.type.getText(sourceFile);
+    const content = `export default function (props: ${compProps}) {
+    return <${compName} {...props}></${compName}>
+}`
+    await fs.writeFile(`./generated-code/lower/${compName}.tsx`, content, 'utf-8')
+    console.log(compName.replace(/^(.)/, it => it.toUpperCase()));
+    await fs.writeFile(`./generated-code/upper/${compName.replace(/^(.)/, it => it.toUpperCase())}.tsx`, content, 'utf-8')
+});
