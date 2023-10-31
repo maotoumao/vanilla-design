ba4e17302dffc626581c07f6c25bee4e17a68381
猫头猫 <31655147+maotoumao@users.noreply.github.com>
2023-10-31 19:43:59 +0800

    Update generate-code.ts

--- src/generate-code.ts
+++ src/generate-code.ts
@@ -1,30 +1,42 @@
 import ts from 'typescript';
 import path from 'path';
-import url from 'url';
+import url, { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 
 
-const srcFile = path.resolve(url.fileURLToPath(import.meta.url), '../react-elements.ts');
-const program = ts.createProgram([srcFile], {
+const typeFilePath = path.resolve(url.fileURLToPath(import.meta.url), '../react-elements.ts');
+
+const program = ts.createProgram([typeFilePath], {
     target: ts.ScriptTarget.ES2016
 });
 
-const sourceFile = program.getSourceFile(srcFile);
+const sourceFile = program.getSourceFile(typeFilePath);
 
-await fs.mkdir("./generated-code/lower", {
+const baseGeneratedPath = path.resolve(fileURLToPath(import.meta.url), '../../g-src/');
+
+function gPath(...args: string[]){
+    return path.resolve(baseGeneratedPath, ...args);
+}
+
+await fs.mkdir(path.resolve(baseGeneratedPath, gPath('lowerCase')), {
     recursive: true
 });
-await fs.mkdir("./generated-code/upper", {
+await fs.mkdir(gPath('upperCase'), {
     recursive: true
 });
 
 (sourceFile.statements[0] as ts.InterfaceDeclaration).members.forEach(async (it: ts.PropertySignature) => {
-    const compName = it.name.getText(sourceFile);
+    let compName = it.name.getText(sourceFile);
+    compName = /^".*"$/.test(compName) ? compName.slice(1, -1): compName;
     const compProps = it.type.getText(sourceFile);
     const content = `export default function (props: ${compProps}) {
     return <${compName} {...props}></${compName}>
 }`
-    await fs.writeFile(`./generated-code/lower/${compName}.tsx`, content, 'utf-8')
-    console.log(compName.replace(/^(.)/, it => it.toUpperCase()));
-    await fs.writeFile(`./generated-code/upper/${compName.replace(/^(.)/, it => it.toUpperCase())}.tsx`, content, 'utf-8')
+    await fs.writeFile(gPath("lowerCase", `${compName}.tsx`), content, 'utf-8')
+    await fs.writeFile(gPath("upperCase", `${compName.replace(/^(.)/, it => it.toUpperCase())}.tsx`), content, 'utf-8')
 });
+
+
+await fs.writeFile(gPath("index.css"), "", "utf-8")
+
+
